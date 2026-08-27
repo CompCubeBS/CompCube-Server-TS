a4a533808bc5b1a21eb00c7af28626e448975bfe
Bela <27pvarga@gmail.com>
2026-08-26 22:05:47 -0400

    do stuff with post request

--- src/routes/reports.route.ts
+++ src/routes/reports.route.ts
@@ -4,5 +4,19 @@ import {playerReportedMatches} from "../../db/schema";
 const router = Router();
 
 router.post("/reports/create/", async (req, res) => {
+    const {
+        matchGuid,
+        reporterGuid
+    } = req.body as Partial<typeof playerReportedMatches.$inferInsert>;
+
+    const date = new Date();
+
+    const report = await db.insert(playerReportedMatches)
+        .values({
+            matchGuid: matchGuid,
+            reporterGuid: reporterGuid,
+            createdAt: date,
+        }).returning();
+
 
 })
